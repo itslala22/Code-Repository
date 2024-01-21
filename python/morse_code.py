f70d0fc8f0bf24dccf9eccc72739c9d0c7e1243e
itslala22 <156817990+itslala22@users.noreply.github.com>
2024-01-21 13:51:17 +0000

    Create morse_code.py

---
+++ python/morse_code.py
@@ -0,0 +1,34 @@
+def morse_translator(text):
+    morse_code_dict = {
+        "A": ".-",
+        "B": "-...",
+        "C": "-.-.",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "V": "...-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+    }
+
+print(morse_translator("HELLO WORLD"))
+
+print(morse_translator("Python"))
+print(morse_translator("Morse Code"))
