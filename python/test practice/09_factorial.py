d2ada2490d90e608e7522429fcc4584552a85a20
itslala22 <156817990+itslala22@users.noreply.github.com>
2024-01-27 11:05:16 +0000

    Add files via upload

---
+++ python/test practice/09_factorial.py
@@ -0,0 +1,39 @@
+"""
+Factorial Calculator Function
+
+Objective:
+Write a function named 'calculate_factorial' to compute the factorial of a number using a for loop.
+
+Function Parameter:
+1. number (integer): A non-negative integer for which the factorial is to be calculated.
+
+Instructions:
+- Use a for loop to calculate the factorial of 'number'.
+- Return the factorial result.
+- Handle edge cases for 0 and negative inputs.
+
+Example Test Cases:
+1. calculate_factorial(5) should return the factorial of 5.
+2. calculate_factorial(0) should return 1.
+3. calculate_factorial(3) should return the factorial of 3.
+4. calculate_factorial(-1) should handle negative input.
+"""
+
+
+def calculate_factorial(number):
+    if number < 0:
+        return "Error message or specific value"
+    
+    if number == 0:
+        return f"output: 1"
+    
+    output = 1
+    for i in range(1,number+1, 1):
+        output = output * i
+    return f"output: {output}"
+
+
+print(calculate_factorial(5))  # Expected output: 120
+print(calculate_factorial(0))  # Expected output: 1
+print(calculate_factorial(3))  # Expected output: 6
+print(calculate_factorial(-1))  # Expected: Error message or specific value
