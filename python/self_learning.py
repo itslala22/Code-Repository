6f7513d6f8b6c2af9dc54cc3bd8f5c85fb95b0bc
itslala22 <156817990+itslala22@users.noreply.github.com>
2024-01-27 14:19:47 +0000

    Add files via upload

---
+++ python/self_learning.py
@@ -0,0 +1,32 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2000
+}
+
+print(thisdict) # {'brand': 'Ford', 'model': 'Mustang', 'year': 2000}
+print(len(thisdict)) # 3
+
+typedict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+print(typedict) # {'brand': 'Ford', 'electric': False, 'year': 1964, 'colors': ['red', 'white', 'blue']}
+print(type(typedict)) # <class 'dict'>
+
+adict = dict(name = "John", age = 36, country = "Norway")
+print(adict) # {'name': 'John', 'age': 36, 'country': 'Norway'}
+
+
+
+
+
+
+
+
+
+
