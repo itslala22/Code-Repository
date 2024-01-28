dab585ee9e934618ca9291e8e795914deeb8278e
itslala22 <156817990+itslala22@users.noreply.github.com>
2024-01-28 13:17:06 +0000

    Update self_learning.py

--- python/self_learning.py
+++ python/self_learning.py
@@ -20,13 +20,39 @@ print(type(typedict)) # <class 'dict'>
 
 adict = dict(name = "John", age = 36, country = "Norway")
 print(adict) # {'name': 'John', 'age': 36, 'country': 'Norway'}
+print(adict.get("name")) #John
+print(adict.keys()) #dict_keys(['name', 'age', 'country'])
+print(adict.values()) #dict_values(['John', 36, 'Norway'])
 
+del adict["country"]
+print(adict) #{'name': 'John', 'age': 36}
 
+for char in adict:
+    print(char) #name age
+    print(adict[char])
+    print(adict.values())
 
+i=0
+while i<6:
+    i=i+1
+    if i == 3:
+        continue
+    print(i)
 
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
+finally:
+   print("is finishied")
 
-
-
-
-
-
+try:
+  print(x)
+except:
+  print("Variable x is not defined")
+else:
+  print("Something else went wrong")
+finally:
+   print("is finishied")
