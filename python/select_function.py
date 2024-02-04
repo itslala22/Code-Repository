484966670cb3e8b0ea204e01dc62e728ecd5fa00
itslala22 <156817990+itslala22@users.noreply.github.com>
2024-02-04 13:03:40 +0000

    Update select_function.py

--- python/select_function.py
+++ python/select_function.py
@@ -1,3 +1,4 @@
+# case 01
 import select
 import socket
 import sys
@@ -14,3 +15,29 @@ server.bind(server_address)
  
 # Listen for incoming connections
 server.listen(5)
+
+
+# case02
+import pymysql
+
+# connect to databse
+db = pymysql.connect(host='localhost',port=3306,user='root',password='password',database='database_name')
+# get cursor
+cur = db.cursor()
+cur.execute('SELECT * FROM table_name')
+# outcome
+results = cur.fetchall()
+# end
+cur.close()
+db.close()
+
+
+# select function
+cur.execute('SELECT * FROM table_name')
+results = cur.fetchall()
+
+cur.execute('SELECT name FROM table_name')
+results = cur.fetchall()
+
+cur.execute('SELECT * FROM table_name WHERE name='John'')
+results = cur.fetchall()
